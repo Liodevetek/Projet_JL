65e08de29759f8f30e104ce8e1930a57ae25d124
Liodevetek <liodevetek@gmail.com>
2022-01-31 12:45:11 +0100

    fiche contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function index(Request $request): Response
+    {
+        $formulaire = $this->createForm(ContactType::class);
+        $formulaire->handleRequest(($request));
+
+        if($formulaire->isSubmitted()){
+
+            $data=$formulaire->getData();
+            $email=$data['email'];
+            $objet=$data['objet'];
+            $message=$data['message'];
+
+            return $this->renderForm('contact/success.html.twig', [
+                'email_uti'=>$email,
+                'objet_mail'=>$objet,
+                'message'=>$message
+            ]);
+
+
+        } else {
+
+
+       return $this->renderForm('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'formAff'=>$formulaire
+        ]);
+    } }
+}
+
+
+
+       
+
+        
+
+
+
+    
+
+
+
+
+
+
+
+
+
+ 
